7c5a80f45bbee938f15e57fc059ed23d8fc3094c
xtina-lt <xtina_lt@outlook.com>
2022-04-11 12:39:36 -0400

    times

--- hellohuman/src/main/java/com/springbootintro/hellohuman/controllers/HelloHumanController.java
+++ hellohuman/src/main/java/com/springbootintro/hellohuman/controllers/HelloHumanController.java
@@ -15,19 +15,30 @@ public class HelloHumanController {
     @RequestMapping("")
 	public String getName(
         @RequestParam(value="first", required=false) String first,
-        @RequestParam(value="last", required=false) String last
+        @RequestParam(value="last", required=false) String last,
+        @RequestParam(value="times", required=true) int times
         ){
 		String full;
+
 		
 		if(first == null && last == null) {
 			full = "Human";
 		} else if (first != null && last != null) {
-			full = String.format("%s  %s", first.toUpperCase(), last.toUpperCase());
+			full = String.format("%s  %s. You're Awesome!", first.toUpperCase(), last.toUpperCase());
 		} else { 
             full = (first == null) ? last.toUpperCase() + ". You're Awesome." : first.toUpperCase() + ". You're Awesome";
 		}
-
-		return String.format("<a href='/'>Go Back</a> <h1>Hello %s!</h1>", full);
+        
+        if (times > 0 ) {
+            String x = "<a href='/'>Go Back</a>";
+            for (int i = 0; i < times ; i++) {
+                x += String.format("<p><h1>Hello %s!</h1></p>", full);
+            }
+            return x;
+        } else {
+            return String.format("<p><a href='/'>Go Back</a> <h1>Hello %s!</h1></p>", full);
+        }
+		
     }
 }
 
