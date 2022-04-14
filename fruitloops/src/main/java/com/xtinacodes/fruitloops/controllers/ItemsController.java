65aa5b653c8271e7d05892caa30dfe884d0ebd3f
xtina-lt <xtina_lt@outlook.com>
2022-04-13 20:13:14 -0400

    session

--- fruitloops/src/main/java/com/xtinacodes/fruitloops/controllers/ItemsController.java
+++ fruitloops/src/main/java/com/xtinacodes/fruitloops/controllers/ItemsController.java
@@ -1,6 +1,9 @@
 package com.xtinacodes.fruitloops.controllers;
 
 import java.util.*;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +14,40 @@ import com.xtinacodes.fruitloops.models.Item;
 public class ItemsController {
 	
 	@RequestMapping("/")
-	public String index(Model m) {
-		ArrayList<Item> fruits = new ArrayList<Item>();
-		fruits.add(new Item("Banana", 2.50));
-		fruits.add(new Item("Blueberries", 8.00));
-		fruits.add(new Item("Mango", 1.50));
+	public String index(HttpSession s, Model m) {
+//		if (s.getAttribute("count")== null) {
+//			s.setAttribute("count", 0);
+//		} else {
+//			int current = (int) s.getAttribute("count");
+//			current++;
+//			s.setAttribute("count", current);
+//		}
 		
+		Object e = s.getAttribute("count");
+		int current = (e  == null) ? 0 : ((int) e + 1);
+		s.setAttribute("count", current);
+	
+
+		ArrayList<Item> fruits = new ArrayList<Item>();
+		fruits.add( new Item("Banana", 2.50) );
+		fruits.add( new Item("Blueberries", 8.00) );
+		fruits.add( new Item("Mango", 1.50) );
+		
+		fruits.add( new Item("Session", (double) current) );
 		m.addAttribute("output", fruits);
+		
 		return "index.jsp";
+	}
+	
+	@RequestMapping("/count")
+	public String counter(HttpSession s, Model m) {
+		Object count = s.getAttribute("count");
+		System.out.println(count);
+		int current = (count  == null) ? 0 : ((int) count + 1);
+		
+		s.setAttribute("count", current);
+		m.addAttribute("current", current);
+
+		return "counter.jsp";
 	}
 }
--- fruitloops/src/main/webapp/WEB-INF/counter.jsp
+++ fruitloops/src/main/webapp/WEB-INF/counter.jsp
@@ -1,52 +1,31 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
 <title>XTINA.CODES</title>
 <link rel="stylesheet" type="text/css" href="/css/style.css">
-<script type="text/javascript" src="/js/script.js"></script>
 <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed&family=Lobster&display=swap" rel="stylesheet">
 </head>
-
 <body>
 	<header>
-	    Fruit Loops
+		Counter
 	</header>
-	
 	<nav>
-	<a href="/count">Counter</a>
-	<a href="/xtina.codes" target="#">xtina.codes</a>
+		<a href="/">Home</a>
+		<a href="/xtina.codes" target="#">xtina.codes</a>
 	</nav>
 	
 	<main>
-		<div>
-			<table>
-				<tr>
-					<th>
-						Fruit
-					</th>
-					<th>
-						Price
-					</th>
-				</tr>
-			    <c:forEach var="i" items="${output}">
-				    <tr>
-				    	<td>
-				    		${i.name}
-				    	</td>
-				    	<td>
-				    		${i.price}
-				    	</td>
-				    </tr>
-			    </c:forEach>
-			</table>
-	    </div>
+		<div class="large">
+			Session Count is :
+			<br>
+			<span class="larger">
+				<c:out value="${current}"/>
+			</span>
+		</div>
 	</main>
 </body>
-
 </html>
